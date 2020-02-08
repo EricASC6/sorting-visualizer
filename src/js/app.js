827e381a0211685c3ae8667f0f2a10832de2dc69
Eric Chen <ericchen9025@gmail.com>
2020-02-08 12:07:56 -0500

    done

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ const array = document.getElementById("visualizer-array");
 const shuffleBtn = document.getElementById("shuffle");
 const newArrayBtn = document.getElementById("new-array");
 const visualizeBtn = document.getElementById("visualize");
-const compareBtn = document.getElementById("compare");
 
 // Current Sorting Algorithm
 let sortAlgo = null;
